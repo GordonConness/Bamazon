a2579286970d5fa8f236d1bb0c07156e8c51bf0a
Gordon Conness <gordon.conness1@gmail.com>
2020-05-20 10:18:01 -0500

    adjustments

--- Customers/schema.sql
+++ Customers/schema.sql
@@ -15,13 +15,13 @@ CREATE TABLE products(
 SELECT * FROM products;
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Uncharted 4", "Video Games", 49.95, 150),
-  ("DOOM", "Video Games", 59.99, 200),
-  ("Crate of Spam", "Food and Drink", 24.50, 50),
-  ("Cool Shades", "Apparel", 75.00, 5),
-  ("Worn Denim Jeans", "Apparel", 54.25, 35),
+VALUES ("World of Warcraft", "Video Games", 49.95, 150),
+  ("Dungeons and Dragons", "Board Games", 59.99, 200),
+  ("Milk", "Food and Drink", 24.50, 50),
+  ("Glasses", "Apparel", 75.00, 5),
+  ("Speedo", "Apparel", 54.25, 35),
   ("Survival Towel", "Necessities", 42.42, 42),
-  ("Bill and Ted's Excellent Adventure", "Films", 15.00, 25),
+  ("Ex Machina", "Films", 15.00, 25),
   ("Mad Max: Fury Road", "Films", 25.50, 57),
   ("Monopoly", "Board Games", 30.50, 35),
   ("Yahtzee", "Board Games", 19.95, 23);
